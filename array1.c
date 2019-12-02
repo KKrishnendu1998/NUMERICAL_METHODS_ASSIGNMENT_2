cec6775bf332cf86a8e42f2a94acae1ad03d43b0
KKrishnendu1998 <57405192+KKrishnendu1998@users.noreply.github.com>
2019-12-03 04:32:37 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,11 @@
+/*programme to create an array of 100 float elements 
+creator : Krishnendu Maji*/
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+float *arr;
+
+int i;
+arr=(float *)malloc(100*sizeof(float));   //this create an array of 100 float elements //
+}
